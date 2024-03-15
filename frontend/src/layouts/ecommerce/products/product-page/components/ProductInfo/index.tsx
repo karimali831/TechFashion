e83839812eede0f9fb019361bbf6029f16cbf710
Karim Ali <karimali831@googlemail.com>
2024-03-15 08:25:16 +0000

    WIP

--- frontend/src/layouts/ecommerce/products/product-page/components/ProductInfo/index.tsx
+++ frontend/src/layouts/ecommerce/products/product-page/components/ProductInfo/index.tsx
@@ -2,7 +2,11 @@ import Autocomplete from "@mui/material/Autocomplete";
 import Grid from "@mui/material/Grid";
 import Icon from "@mui/material/Icon";
 import { useState } from "react";
-import { useAddProductToCartMutation, useGetCartQuery, useUpdateProductQuantityMutation } from "src/api/cartApi";
+import {
+    useAddProductToCartMutation,
+    useGetCartQuery,
+    useUpdateProductQuantityMutation,
+} from "src/api/cartApi";
 import { useGetProductQuery } from "src/api/productApi";
 import { ActionButton } from "src/components/Buttons/ActionButton";
 import MDBadge from "src/components/MDBadge";
@@ -23,7 +27,8 @@ function ProductInfo({ item }: IProps): JSX.Element {
     const [quantity, setQuantity] = useState<number>(1);
     const [variations, setVariations] = useState<IProductVariantObj[]>([]);
 
-    const [addProductToCart, { isLoading: adding }] = useAddProductToCartMutation();
+    const [addProductToCart, { isLoading: adding }] =
+        useAddProductToCartMutation();
     const [updateProductQuantity] = useUpdateProductQuantityMutation();
 
     const { data: cart } = useGetCartQuery();
@@ -35,13 +40,20 @@ function ProductInfo({ item }: IProps): JSX.Element {
     const itemsInCart: ICartProductDetail[] = cart?.products ?? [];
 
     const itemInCart = itemsInCart.filter((x) =>
-        item.some((i) => i.id === x.productId && (!i.productVariantId || i.productVariantId === x.productVariantId))
+        item.some(
+            (i) =>
+                i.id === x.productId &&
+                (!i.productVariantId ||
+                    i.productVariantId === x.productVariantId)
+        )
     )[0];
 
     const itemVariantInCart =
         itemInCart &&
         itemInCart.variationsList.filter((x) =>
-            variations.some((y) => y.attribute === x.attribute && y.value === x.value)
+            variations.some(
+                (y) => y.attribute === x.attribute && y.value === x.value
+            )
         );
 
     // console.log("p1", itemInCart?.variationsList);
@@ -84,10 +96,18 @@ function ProductInfo({ item }: IProps): JSX.Element {
     };
 
     const updateVariant = (key: string, value: string) => {
-        setVariations({
-            ...variations,
-            [key]: value,
-        });
+        const exists = variations.filter((x) => x.attribute === key)[0];
+
+        if (exists) {
+            setVariations(
+                variations.map((v) =>
+                    v.attribute === key ? { ...v, value } : v
+                )
+            );
+        } else {
+            const newState = [...variations, { attribute: key, value }];
+            setVariations(newState);
+        }
     };
 
     return (
@@ -114,30 +134,80 @@ function ProductInfo({ item }: IProps): JSX.Element {
                     {item[0].price}
                 </MDTypography>
             </MDBox>
-            <MDBadge variant="contained" color="success" badgeContent="in stock" container />
+            <MDBadge
+                variant="contained"
+                color="success"
+                badgeContent="in stock"
+                container
+            />
             <MDBox mt={3} mb={1} ml={0.5}>
-                <MDTypography variant="button" fontWeight="regular" color="text">
+                <MDTypography
+                    variant="button"
+                    fontWeight="regular"
+                    color="text"
+                >
                     Description
                 </MDTypography>
             </MDBox>
             <MDBox component="ul" m={0} pl={4} mb={2}>
-                <MDBox component="li" color="text" fontSize="1.25rem" lineHeight={1}>
-                    <MDTypography variant="body2" color="text" fontWeight="regular" verticalAlign="middle">
-                        The most beautiful curves of this swivel stool adds an elegant touch to any environment
+                <MDBox
+                    component="li"
+                    color="text"
+                    fontSize="1.25rem"
+                    lineHeight={1}
+                >
+                    <MDTypography
+                        variant="body2"
+                        color="text"
+                        fontWeight="regular"
+                        verticalAlign="middle"
+                    >
+                        The most beautiful curves of this swivel stool adds an
+                        elegant touch to any environment
                     </MDTypography>
                 </MDBox>
-                <MDBox component="li" color="text" fontSize="1.25rem" lineHeight={1}>
-                    <MDTypography variant="body2" color="text" fontWeight="regular" verticalAlign="middle">
+                <MDBox
+                    component="li"
+                    color="text"
+                    fontSize="1.25rem"
+                    lineHeight={1}
+                >
+                    <MDTypography
+                        variant="body2"
+                        color="text"
+                        fontWeight="regular"
+                        verticalAlign="middle"
+                    >
                         Memory swivel seat returns to original seat position
                     </MDTypography>
                 </MDBox>
-                <MDBox component="li" color="text" fontSize="1.25rem" lineHeight={1}>
-                    <MDTypography variant="body2" color="text" fontWeight="regular" verticalAlign="middle">
+                <MDBox
+                    component="li"
+                    color="text"
+                    fontSize="1.25rem"
+                    lineHeight={1}
+                >
+                    <MDTypography
+                        variant="body2"
+                        color="text"
+                        fontWeight="regular"
+                        verticalAlign="middle"
+                    >
                         Comfortable integrated layered chair seat cushion design
                     </MDTypography>
                 </MDBox>
-                <MDBox component="li" color="text" fontSize="1.25rem" lineHeight={1}>
-                    <MDTypography variant="body2" color="text" fontWeight="regular" verticalAlign="middle">
+                <MDBox
+                    component="li"
+                    color="text"
+                    fontSize="1.25rem"
+                    lineHeight={1}
+                >
+                    <MDTypography
+                        variant="body2"
+                        color="text"
+                        fontWeight="regular"
+                        verticalAlign="middle"
+                    >
                         Fully assembled! No assembly required
                     </MDTypography>
                 </MDBox>
@@ -147,12 +217,17 @@ function ProductInfo({ item }: IProps): JSX.Element {
                     {products.variants
                         .filter((x) => x.productId === item[0].id)
                         .map((variant, idx) => {
-                            const selectedVariant = variations.filter((x) => x.attribute === variant.attribute)[0]
-                                ?.value;
+                            const selectedVariant = variations.filter(
+                                (x) => x.attribute === variant.attribute
+                            )[0]?.value;
 
                             return (
                                 <Grid key={idx} item xs={12} lg={5}>
-                                    <MDBox mb={1.5} lineHeight={0} display="inline-block">
+                                    <MDBox
+                                        mb={1.5}
+                                        lineHeight={0}
+                                        display="inline-block"
+                                    >
                                         <MDTypography
                                             component="label"
                                             variant="button"
@@ -163,12 +238,26 @@ function ProductInfo({ item }: IProps): JSX.Element {
                                         </MDTypography>
                                     </MDBox>
                                     <Autocomplete
-                                        value={selectedVariant ?? variant.options[0]}
-                                        options={variant.options}
-                                        onChange={(e: React.SyntheticEvent, value: string) =>
-                                            updateVariant(variant.attribute, value)
+                                        value={
+                                            selectedVariant ??
+                                            variant.options[0]
                                         }
-                                        renderInput={(params) => <MDInput {...params} variant="standard" />}
+                                        options={variant.options}
+                                        onChange={(
+                                            e: React.SyntheticEvent,
+                                            value: string
+                                        ) =>
+                                            updateVariant(
+                                                variant.attribute,
+                                                value
+                                            )
+                                        }
+                                        renderInput={(params) => (
+                                            <MDInput
+                                                {...params}
+                                                variant="standard"
+                                            />
+                                        )}
                                     />
                                 </Grid>
                             );
@@ -176,8 +265,15 @@ function ProductInfo({ item }: IProps): JSX.Element {
 
                     <Grid item xs={12} lg={5}>
                         <MDBox mb={1.5} lineHeight={0}>
-                            <MDTypography component="label" variant="button" color="text" fontWeight="regular">
-                                Quantity {itemInCart && `(${itemInCart.quantity} in cart)`}
+                            <MDTypography
+                                component="label"
+                                variant="button"
+                                color="text"
+                                fontWeight="regular"
+                            >
+                                Quantity{" "}
+                                {itemInCart &&
+                                    `(${itemInCart.quantity} in cart)`}
                             </MDTypography>
                         </MDBox>
                         <MDInput
@@ -186,8 +282,9 @@ function ProductInfo({ item }: IProps): JSX.Element {
                                 min: 1,
                                 max: 10,
                                 style: { width: 150 },
-                                onChange: (e: React.ChangeEvent<HTMLInputElement>) =>
-                                    setQuantity(Number(e.target.value)),
+                                onChange: (
+                                    e: React.ChangeEvent<HTMLInputElement>
+                                ) => setQuantity(Number(e.target.value)),
                             }}
                             value={quantity}
                             variant="standard"
@@ -197,7 +294,12 @@ function ProductInfo({ item }: IProps): JSX.Element {
             </MDBox>
             <MDBox mt={3}>
                 <Grid item xs={12} lg={5} container>
-                    <ActionButton disabled={quantity === 0} loading={adding} text="add to cart" onClick={addToCart} />
+                    <ActionButton
+                        disabled={quantity === 0}
+                        loading={adding}
+                        text="add to cart"
+                        onClick={addToCart}
+                    />
                 </Grid>
             </MDBox>
         </MDBox>
